84110816dfdfe0f426bdd76ff0c11bff50348e6c
kelvin wepo <kelvinwepo7710@gmail.com>
2023-03-10 13:50:52 +0300

    ft:this commit adds test.py file

---
+++ tests.py
@@ -0,0 +1,19 @@
+from three import choose_three
+
+
+if __name__ == '__main__':
+
+
+    # tests
+    print(choose_three("You have no idea how to approach this problem.",
+          "Send an email to the TAs",
+          "Go to office hours",
+          "Post the problem online, there's no way I'll be caught"))
+    print(choose_three("You must decide the fate of the galaxy.",
+          "Destroy the robots",
+          "Control the robots",
+          "Merge with the robots?"))
+    print(choose_three("Choose a house.",
+          "Black Eagles",
+          "Slytherin",
+          "Lannister"))
